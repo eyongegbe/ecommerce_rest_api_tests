0dbc6db13ba178e4b0b1edc2028c280b0870c61b
Lambert Egbe <lambert.egbe@consulterysolutions.co.uk>
2023-06-20 17:42:22 +0100

    removing a test

--- tests/test_healthcheck.py
+++ tests/test_healthcheck.py
@@ -4,14 +4,3 @@ from woocommerce import API
 
 def test_framework_healthcheck():
     logger.info('just checking framework is wired correctly')
-
-
-def test_woo_commerce():
-    wcapi = API(
-        url="http://localhost/testsite/wp-json/wc/v3/",
-        consumer_key="ck_193946c3c441358f82d409aa49a4437e70fab5e6",
-        consumer_secret="cs_abeca33e603806408753d245c95cdbf29f3b8181",
-        version="wc/v3"
-    )
-    response = wcapi.get("products")
-    print(response.json())
